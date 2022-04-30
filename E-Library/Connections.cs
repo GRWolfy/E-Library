a8e89929811e4e2cbaf4580e769dccc3e83ef40d
GRWolfy <granthreggwolf@gmail.com>
2022-04-30 17:22:54 +0800

    Add project files.

---
+++ E-Library/Connections.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Library
+{
+    public class Connection
+    {
+        public static SqlConnection con;
+        private static string dbconnect = "Data Source=WOLF;Initial Catalog=OrnamentalSystem;Integrated Security=True"; //connection string 
+
+        public static void DB()
+        {
+            try
+            {
+                con = new SqlConnection(dbconnect);
+                con.Open();
+            }
+            catch (Exception e)
+            {
+                con.Close();
+                MessageBox.Show(e.Message);
+            }
+        }
+    }
+}
